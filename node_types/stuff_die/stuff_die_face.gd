4e1877b7c69f9ab307925bd28dd08c1acf6d22cd
Adam Howard <ahoward0920@gmail.com>
2025-04-05 13:39:17 -0500

    adding interface for working with faces

--- node_types/stuff_die/stuff_die_face.gd
+++ node_types/stuff_die/stuff_die_face.gd
@@ -7,3 +7,9 @@ extends Resource
 func _init(p_pips: Dictionary[Enums.PIP_TYPE,int] = {}, p_autolocking: bool = false) -> void:
 	pips = p_pips
 	autolocking = p_autolocking
+
+func get_pips(pip_type: Enums.PIP_TYPE):
+	return pips[pip_type] if pip_type in pips else 0
+	
+func set_pips(pip_type: Enums.PIP_TYPE, pip_value: int):
+	pips[pip_type] = pip_value
