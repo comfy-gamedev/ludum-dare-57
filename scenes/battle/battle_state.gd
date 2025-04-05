7e880ee23d67e50973d69c8336a4bbd3d193d9c5
Apples <2352020+apples@users.noreply.github.com>
2025-04-05 14:24:47 -0500

    Initial Battle WIP

---
+++ scenes/battle/battle_state.gd
@@ -0,0 +1,14 @@
+class_name BattleState
+extends RefCounted
+
+var deck: Array[StuffDie]
+var hand: Array[StuffDie]
+
+var mana: int
+var rerolls: int
+
+var roll_results: Array[RollResult]
+
+class RollResult:
+	var die: StuffDie
+	var face: int
---
+++ scenes/battle/player_stats.gd
@@ -0,0 +1,7 @@
+class_name PlayerStats
+extends RefCounted
+
+var max_health: int
+var health: int
+
+var equipment: Array[StuffDie]
