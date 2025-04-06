53c24fd7bd8b5f93175e3514f3b82b9d84f5efad
WiredOverload <mike.everywhere@gmail.com>
2025-04-05 21:51:02 -0500

    partial navigation for Angus

--- scenes/navigation/navigation.gd
+++ scenes/navigation/navigation.gd
@@ -14,26 +14,6 @@ var button_type := preload("res://scenes/navigation/location_node.tscn")
 func _ready() -> void:
 	get_next_nodes(Vector2i(4, 0))
 	
-	#var width
-	#var last_nodes := []
-	#var current_nodes := []
-	#
-	#for i in LAYERS:
-		#width = randi_range(3, MAX_WIDTH)
-		#for j in width:
-			#var loc_button := button_type.instantiate()
-			#loc_button.position = Vector2(480 - ((W_SPACING / 2) * (width - 1)) + (W_SPACING * (j - 1)), 50 + (H_SPACING * i))
-			#container.add_child(loc_button)
-			#current_nodes.append(loc_button)
-			#for k in last_nodes:
-				#k.next_nodes.append(loc_button)
-		#last_nodes = current_nodes
-	#
-	#for i in container.get_children():
-		#for j in i.next_nodes:
-			#var line = Line2D.new()
-			#line.add_point(i.position)
-			#line.add_point(j.position)
 
 func get_next_nodes(coord: Vector2i) -> Array[Vector2i]:
 	var current_pipe := coord + Vector2i(0, 1)
@@ -42,50 +22,32 @@ func get_next_nodes(coord: Vector2i) -> Array[Vector2i]:
 	#move left
 	while status != "done":
 		match tilemap.get_cell_atlas_coords(current_pipe).x:
-			0:#straight down
+			2:#straight down
 				nodes.append(current_pipe + Vector2i(0, 1))
 				status = "done"
-			1:
+			1, 8:#sideways pipe, T junction up
 				if status == "left":
 					current_pipe += Vector2i.LEFT
 				else:
 					current_pipe += Vector2i.RIGHT
-			2:#right angle
+			4:#right angle left
 				current_pipe += Vector2i.LEFT
-			3:
+			6:#right angle right
 				current_pipe += Vector2i.RIGHT
-			4, 5:
+			3, 5, 9, 10:#right angles down, T junction sideways
 				nodes.append(current_pipe + Vector2i(0, 1))
 				if status == "left":
 					status = "right"
 					current_pipe = coord + Vector2i(1, 1)
 				else:
 					status = "done"
-			6:#T junction up
-				if status == "left":
-					current_pipe += Vector2i.LEFT
-				else:
-					current_pipe += Vector2i.RIGHT
-			7:
+			7, 0:#T junction down, 4 cross
 				nodes.append(current_pipe + Vector2i(0, 1))
 				if status == "left":
 					current_pipe += Vector2i.LEFT
 				else:
 					current_pipe += Vector2i.RIGHT
-			8, 9:
-				nodes.append(current_pipe + Vector2i(0, 1))
-				if status == "left":
-					status = "right"
-					current_pipe = coord + Vector2i(1, 1)
-				else:
-					status = "done"
-			10:#4 cross
-				nodes.append(current_pipe + Vector2i(0, 1))
-				if status == "left":
-					current_pipe += Vector2i.LEFT
-				else:
-					current_pipe += Vector2i.RIGHT
-		
+	
 	return nodes
 
 func _unhandled_input(event: InputEvent) -> void:
