5d26340b870419a8aa7dbab7e54c7fc8ededf4e8
WiredOverload <mike.everywhere@gmail.com>
2025-04-07 21:04:44 -0500

    reset on win as well

--- scenes/you_win/u_win.gd
+++ scenes/you_win/u_win.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 func _ready() -> void:
 	MusicMan.music(preload("res://assets/music/Daisy Chains (Lose Theme) Final.ogg"))
+	Globals.reset()
 
 func _on_button_pressed() -> void:
 	SceneGirl.change_scene("res://scenes/main_menu/main_menu.tscn")
